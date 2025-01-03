2df70a2094fdb7fd8058ad2502e51b719746bc5b
deepanshu <deepanshukn7@gmail.com>
2025-01-03 12:35:20 +0530

    initial commit

---
+++ app/src/main/java/com/example/expensemanager/StatsFragment.java
@@ -0,0 +1,17 @@
+package com.example.expensemanager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+
+public class StatsFragment extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_stats);
+
+    }
+}
